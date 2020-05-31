31d424b1fef9e665221095dc7daa1e537999e25f
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-05-31 21:14:07 +0200

    17.4 HQL and Named Queries

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -9,11 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CompanyDaoTestSuite {
     @Autowired
     CompanyDao companyDao;
+    @Autowired
+    EmployeeDao employeeDao;
+
 
     @Test
     public void testSaveManyToMany(){
@@ -60,4 +66,44 @@ public class CompanyDaoTestSuite {
         //    //do nothing
         //}
     }
+    @Test
+    public void testRetrieveEmployeeBySurname(){
+        //Given
+        Employee emp1 = new Employee("Kate", "Smith");
+        Employee emp2 = new Employee("Anne", "Brown");
+        Employee emp3 = new Employee("Robert", "Kinney");
+
+        employeeDao.save(emp1);
+        employeeDao.save(emp2);
+        employeeDao.save(emp3);
+
+        //When
+        List<Employee> employeeBySurname = employeeDao.retrieveEmployeeBySurname("Smith");
+
+        //Then
+        Assert.assertNotEquals(0, employeeBySurname.size());
+
+
+
+    }
+
+    @Test
+    public void testRetrieveByFirstThreeLettersOfCompanyName(){
+        //Given
+        Company comp1 = new Company("Seagate");
+        Company comp2 = new Company("Samsung");
+        Company comp3 = new Company("Bosch");
+
+        companyDao.save(comp1);
+        companyDao.save(comp2);
+        companyDao.save(comp3);
+
+        //When
+        List<Company> foundCompanies = companyDao.retrieveByFirstThreeLettersOfCompanyName("Sea");
+
+        //Then
+        Assert.assertNotEquals(0, foundCompanies.size());
+
+    }
+
 }
