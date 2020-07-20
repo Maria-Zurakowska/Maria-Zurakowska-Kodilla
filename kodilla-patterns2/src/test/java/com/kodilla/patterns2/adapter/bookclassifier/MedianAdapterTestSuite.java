4d663fa00fbe0a7038bca697428b96e863dac07a
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-07-20 11:33:23 +0200

    24.5 Adapter pattern

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -1,6 +1,13 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
+import com.kodilla.patterns2.adapter.bookclasifier.MedianAdapter;
+import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 public class MedianAdapterTestSuite {
 
@@ -8,10 +15,17 @@ public class MedianAdapterTestSuite {
     @Test
     public void publicationYearMedianTest(){
 
-        // w którym stwórz przykładowy zbiór Set<Book> i wywołaj metodę
-        // publicationYearMedian() klasy MedianAdapter.
-        // Napisz asercję sprawdzającą czy wynik testu jest zgodny
-        // z oczekiwaniami.
-
+        //Given
+        Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> books = new HashSet<>();
+        Set<com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> booksSet = new HashSet<>();
+        IntStream.range(0, 9).forEach(i -> {
+            booksSet.add(new com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book("Author" + i, "Title" + i, 1991 + i));
+            booksSet.add(new com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book("Author" + i, "Title" + i, 1991 + i));
+        });
+        MedianAdapter medianAdapter = new MedianAdapter();
+        //When
+        int result = medianAdapter.publicationYearMedian(books);
+        //Then
+        Assert.assertEquals(0,result);
     }
 }
