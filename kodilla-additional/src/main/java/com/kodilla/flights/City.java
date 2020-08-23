1f7fe259291c9d16669b501207ed773719a33bdb
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-23 09:38:25 +0200

    removal of unused content

--- kodilla-additional/src/main/java/com/kodilla/flights/City.java
+++ kodilla-additional/src/main/java/com/kodilla/flights/City.java
@@ -1,11 +1,5 @@
 package com.kodilla.flights;
 
-// lista mozliwych wartosci
-//drukowane litery
-// City typ
-// moze przyjac 1 z 4 wartosci
-// tylko tekst (ew. cyfra dalej)
-
 public enum City {
     WAW,
     LAX,
