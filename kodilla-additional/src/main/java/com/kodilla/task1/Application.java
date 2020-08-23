182c8193420824d4c07d41cb16a32d23844d49c9
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-23 09:42:30 +0200

    removal of comments

--- kodilla-additional/src/main/java/com/kodilla/task1/Application.java
+++ kodilla-additional/src/main/java/com/kodilla/task1/Application.java
@@ -4,12 +4,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        // klasa utworzy nowy obiekt klasy FigureCollector
         FigureCollector figcol = new FigureCollector();
-
-        // a następnie utwórz przykładowe figury i dodaj je do
-        // `FigureCollectora`. Sprawdź jak wygąda wynik wywołania
-        // metody `showFigures()
 
         Figure2D circle = new Circle("Circle ", 5);
         Figure2D rectangle = new Rectangle("Rectangle ", 4,8);
