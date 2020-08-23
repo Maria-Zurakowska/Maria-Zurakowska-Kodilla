d2d31c42a1ee624425b62c9fdca9bfc310c23168
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-23 11:05:33 +0200

    removal of unused comments and import

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,4 @@
 package com.kodilla.stream;
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
@@ -13,8 +11,9 @@ public class StreamMain {
     public static void main(String[] args) {
 
         Forum theForum = new Forum();
+
 // Integer -userId, String - username
-        Map<Integer, ForumUser> theResultMapOfForumUsers = theForum.getUserList().stream()
+        Map<Integer,ForumUser> theResultMapOfForumUsers = theForum.getUserList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
                 .filter(forumUser -> 2020 - forumUser.getBirthdate().getYear() >= 20)
                 .filter(forumUser -> forumUser.getNumberOfPosts() >= 1)
@@ -29,12 +28,9 @@ public class StreamMain {
         double asDouble = theForum.getUserList().stream()
                 .filter(user -> user.getBirthdate().getYear() < LocalDate.now().getYear() - 20)
 
-// Podpytaj jeszcze raz Andrzeja o te 3 niżej
                 .map(u -> LocalDate.now().getYear() - u.getBirthdate().getYear())
                 .mapToInt(n -> n)
                 .average().getAsDouble();
-
-
     }
 
     private static ForumUser displayUser(ForumUser us) {
