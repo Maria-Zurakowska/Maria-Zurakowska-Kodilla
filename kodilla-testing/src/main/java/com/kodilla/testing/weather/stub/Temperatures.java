42999bdd8e80321c81de596eb786cffd25aa7436
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-23 11:12:12 +0200

    fixing typo

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/Temperatures.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/Temperatures.java
@@ -2,8 +2,9 @@ package com.kodilla.testing.weather.stub;
 import java.util.HashMap;
 
 public interface Temperatures {
+
     //first element of the map is an identifier of weather station
-    //second element of the map is a temperature in Celcius degrees
+    //second element of the map is a temperature in Celsius degrees
 
     HashMap<Integer, Double> getTemperatures();
 }
