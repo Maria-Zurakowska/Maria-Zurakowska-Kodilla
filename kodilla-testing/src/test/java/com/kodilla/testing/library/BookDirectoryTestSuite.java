f06177f0de303a535a6937bb6796799248e853ee
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-23 10:59:03 +0200

    removal of unused comments

--- kodilla-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
@@ -11,10 +11,10 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
 public class BookDirectoryTestSuite {
-    @Test //czy lista książek spełniających warunek wyszukiwania jest poprawna
+    @Test
     public void testListBooksWithConditionsReturnList(){
         //Given
-// tworzymy mock (atrapę klasy) dla interfejsu LibraryDatabase
+
         LibraryDatabase libraryDatabaseMock = mock(LibraryDatabase.class);
         BookLibrary bookLibrary = new BookLibrary(libraryDatabaseMock);
         List<Book> resultListOfBooks = new ArrayList<Book>();
@@ -27,8 +27,8 @@ public class BookDirectoryTestSuite {
         resultListOfBooks.add(book3);
         resultListOfBooks.add(book4);
         // ->instruujemy MOCK, jak ma się zachować, gdy wywołam metodę .listBooksWithCondition
-        //na MOCKu wywołuję metodę, daję parametr Secret. Jeśli on będzie to zwracam listę książek
-        //jeśli w liście są książki, które zaczynają się od słowa Secret to zwróc wtedy całą listę książek
+        //na MOCKu wywołuję metodę, daję parametr Secret. Jeśli on będzie, to zwracam listę książek
+        //jeśli w liście są książki, które zaczynają się od słowa Secret, to zwróc wtedy całą listę książek
         when(libraryDatabaseMock.listBooksWithCondition("Secret")).thenReturn(resultListOfBooks);
 
         //When -->wywołujemy metodę listBookWithCondition(String titleFragment) obiektu bookLibrary
@@ -37,7 +37,7 @@ public class BookDirectoryTestSuite {
         //Then --sprawdzamy, czy zwrócone zostały cztery tytuły książek (tyle właśnie przygotowaliśmy)
         assertEquals(4, theListOfBooks.size());
     }
-//metoda, które generuje listę książek o dowolnej liczbie tytułów
+
     private List<Book> generateListOfNBooks(int booksQuantity){
         List<Book> resultList = new ArrayList<Book>();
         for (int n = 1; n <= booksQuantity ; n++) {
@@ -46,7 +46,7 @@ public class BookDirectoryTestSuite {
         }
         return resultList;
     }
-    @Test //czy liczba tytułów jest większa niż 20
+    @Test
     public void testListBooksWithConditionMoreThan20(){
         // Given
         LibraryDatabase libraryDatabaseMock = mock(LibraryDatabase.class);
@@ -71,24 +71,20 @@ public class BookDirectoryTestSuite {
         assertEquals(0, theListOfBooks40.size());
 
     }
-    @Test //czy wyszukiwany fragment jest krótszy niż 3 znaki
+    @Test
     public void testListBooksWithConditionFragmentShorterThan3(){
         //Given
         LibraryDatabase libraryDatabaseMock = mock(LibraryDatabase.class);
         BookLibrary bookLibrary = new BookLibrary(libraryDatabaseMock);
-// tworzę listę 10 książek
+
         List<Book> resultOf10Books = generateListOfNBooks(10);
-//zwróć listę 10 książek (thenReturn(resultOf10Books), gdy wywołam metodę .listBooksWithCondition
         when(libraryDatabaseMock.listBooksWithCondition(anyString())).thenReturn(resultOf10Books);
 
-        //When -->wywołuję metodę z argumentem An
+        //When
         List<Book> theListOf10Books = bookLibrary.listBooksWithCondition("An");
 
         //Then
-
-// sprawdzam, czy lista jest pusta (0). Ma być pusta, gdy dł. ciągu znaku jest mniejsza niż trzy znaki
         assertEquals(0,theListOf10Books.size());
-// sprawdzam, czy metoda .listBooksWith... na pewno nie została ani razu wywołana
         verify(libraryDatabaseMock,times(0)).listBooksWithCondition(anyString());
     }
 
